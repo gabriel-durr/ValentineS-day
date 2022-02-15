0fe95d6829becdb31f784064f97e725a035e294e
Gabriel Durr <gabriel.durr@outlook.com>
2022-02-15 15:20:49 -0300

    first

---
+++ treino.js
@@ -0,0 +1,46 @@
+
+
+function redHeart() {
+    
+    let r = document.getElementById("red")
+    let color = r.style.backgroundColor = "red"
+     color = r.style.boxShadow = "2px 0px 70px 17px red"
+    r = color 
+    
+    
+}
+
+setTimeout(redHeart, 2000)
+
+
+
+function yellowHeart() {
+
+    let r = document.getElementById("yellow")
+    let color = r.style.backgroundColor = "yellow";
+    color = r.style.boxShadow = "2px 0px 70px 17px yellow"
+    r = color
+}
+
+setTimeout(yellowHeart, 3500)
+
+
+
+
+function greenHeart() {
+
+    let r = document.getElementById("green")
+    let color = r.style.backgroundColor = "green"
+     color = r.style.boxShadow = "2px 0px 70px 17px green"
+    r = color
+}
+
+setTimeout(greenHeart, 4500)
+
+
+function loveText() {
+        let t = document.getElementById("text")
+        let ixibirTexto = t.innerHTML = "Te amo Larissa 👩🏾‍❤️‍👨🏻"
+}
+
+setTimeout(loveText, 5500)
